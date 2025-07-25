1177bb68127749bca414020f208865a5a2ef1cce
Justin Hooker <justinhooker1@gmail.com>
2025-07-25 11:25:49 -0400

    removed unnecessary  files from project

--- FlashlightToolLoader/obj/Debug/netstandard2.1/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace FlashlightToolLoader
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "Sniper1_1.FlashlightToolLoader";
-        public const string PLUGIN_NAME = "FlashlightToolLoader";
-        public const string PLUGIN_VERSION = "1.0.2";
-    }
-}
